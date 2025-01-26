584375d395d14ac82a7aa9a2a683cfdf168c041b
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-26 13:11:17 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,4 @@
 def call(String projectName, String ImageTag, String DockerhubUser){
   sh "docker build -t ${DockerhubUser}/${projectName}:${ImageTag} ."
+  echo "Built a docker Image Sucessfully."
 }
