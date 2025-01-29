f02edea4bc2ac24c9e50bb18c671e017513e9d2d
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-30 00:47:48 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,9 +1,4 @@
 def call(String url, String branch){
-  withCredentials([usernamePassword(credentialsId: 'Github-Credentials', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
-                    sh '''
-                        # Clone using HTTPS with a PAT
-                       git clone --branch ${branch} https://$GIT_USERNAME:$GIT_TOKEN@github.com/eyepatch5263/codilio.git
-  '''
-  }
+sh "git clone --branch ${branch} ${url}"
  echo "cloned the github repo sucessfully"
 }
