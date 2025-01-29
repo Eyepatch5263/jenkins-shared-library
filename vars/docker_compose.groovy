769c62028e9bde10b32c74c10ff5ae090daeaf7d
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-29 23:35:01 +0530

    Update docker_compose.groovy

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -1,7 +1,8 @@
 def call(String Projectname){
   sh '''
-  cd ${Projectname}
-  docker compose down && docker compose up -d
+  cd ${Projectname} &&
+            docker compose down &&
+            docker compose up -d
   '''
   echo "Container started successfully."
 }
