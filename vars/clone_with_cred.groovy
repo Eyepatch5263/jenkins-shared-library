fc71b9dd277772d9da55f9698c7731731a1dd230
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-31 00:42:09 +0530

    Update clone_with_cred.groovy

--- vars/clone_with_cred.groovy
+++ vars/clone_with_cred.groovy
@@ -1,20 +1,19 @@
-def call(String Projectname, String branch ){
-  withCredentials([usernamePassword(credentialsId: 'Github-Credentials', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
-                
-                sh """
-                if [ -d "${Projectname}" ]; then
-                        rm -rf ${Projectname}
-                    fi
-                    # Prevent leaking credentials
-                     # Configure git to use the credentials
-                        git config --global credential.helper store --file=/var/lib/jenkins/.git-credentials
-                        echo "https://${GIT_USERNAME}:${GIT_TOKEN}@github.com" > /var/lib/jenkins/.git-credentials
-                        
-                        # Clone the repository
-                        git clone --branch main https://github.com/Eyepatch5263/codilio.git
-                        
-                        # Clean up credentials
-                        rm -f /var/lib/jenkins/.git-credentials
-                """
-            }
+def call(String Projectname, String branch) {
+    withCredentials([usernamePassword(credentialsId: 'Github-Credentials', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
+        sh '''
+            if [ -d "$Projectname" ]; then
+                rm -rf "$Projectname"
+            fi
+
+            # Configure git credentials
+            git config --global credential.helper store --file=/var/lib/jenkins/.git-credentials
+            echo "https://${GIT_USERNAME}:$GIT_TOKEN@github.com" > /var/lib/jenkins/.git-credentials
+
+            # Clone the repository
+            git clone --branch $branch https://github.com/Eyepatch5263/codilio.git
+
+            # Clean up credentials
+            rm -f /var/lib/jenkins/.git-credentials
+        '''
+    }
 }
