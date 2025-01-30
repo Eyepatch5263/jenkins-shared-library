4c8790a7db284b47b6be6c87c2bb1e075a682746
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-31 00:44:57 +0530

    Update clone_with_cred.groovy

--- vars/clone_with_cred.groovy
+++ vars/clone_with_cred.groovy
@@ -1,19 +1,12 @@
 def call(String Projectname, String branch) {
     withCredentials([usernamePassword(credentialsId: 'Github-Credentials', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
-        sh '''
-            if [ -d "$Projectname" ]; then
-                rm -rf "$Projectname"
+        sh """
+            if [ -d '${Projectname}' ]; then
+                rm -rf '${Projectname}'
             fi
 
-            # Configure git credentials
-            git config --global credential.helper store --file=/var/lib/jenkins/.git-credentials
-            echo "https://${GIT_USERNAME}:$GIT_TOKEN@github.com" > /var/lib/jenkins/.git-credentials
-
             # Clone the repository
-            git clone --branch $branch https://github.com/Eyepatch5263/codilio.git
-
-            # Clean up credentials
-            rm -f /var/lib/jenkins/.git-credentials
-        '''
+            git clone --branch '${branch}' https://\${GIT_USERNAME}:\${GIT_TOKEN}@github.com/Eyepatch5263/${Projectname}.git
+        """
     }
 }
