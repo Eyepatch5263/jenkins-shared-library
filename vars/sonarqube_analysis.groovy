806c4e14d8516e6266422438e24199b1d7ce2b51
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-31 17:24:38 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,11 +1,17 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
+    echo "Starting SonarQube Analysis..."
+    
     withSonarQubeEnv("${SonarQubeAPI}") {
+        sh 'echo "SonarQube Server: $SONAR_HOST_URL"'
+        
         sh """
-            ${SONAR_SCANNER_HOME}/bin/sonar-scanner \
-            -Dsonar.projectName='${Projectname}' \
-            -Dsonar.projectKey='${ProjectKey}' \
-            -Dsonar.host.url='$SONAR_HOST_URL' \
+            ${SONAR_SCANNER_HOME}/bin/sonar-scanner \\
+            -Dsonar.projectName="${Projectname}" \\
+            -Dsonar.projectKey="${ProjectKey}" \\
+            -Dsonar.host.url="${SonarQubeAPI}" \\
             -X
         """
     }
+    
+    echo "SonarQube Analysis Completed!"
 }
