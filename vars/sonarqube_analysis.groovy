bc7e759163d11bd367593da6b8f8d4d76e354820
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-31 17:29:21 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,17 +1,23 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
-    echo "Starting SonarQube Analysis..."
+// def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
+//     echo "Starting SonarQube Analysis..."
     
-    withSonarQubeEnv("${SonarQubeAPI}") {
-        sh 'echo "SonarQube Server: $SONAR_HOST_URL"'
+//     withSonarQubeEnv("${SonarQubeAPI}") {
+//         sh 'echo "SonarQube Server: $SONAR_HOST_URL"'
         
-        sh """
-            ${SONAR_SCANNER_HOME}/bin/sonar-scanner \\
-            -Dsonar.projectName="${Projectname}" \\
-            -Dsonar.projectKey="${ProjectKey}" \\
-            -Dsonar.host.url="${SonarQubeAPI}" \\
-            -X
-        """
-    }
+//         sh """
+//             ${SONAR_SCANNER_HOME}/bin/sonar-scanner \\
+//             -Dsonar.projectName="${Projectname}" \\
+//             -Dsonar.projectKey="${ProjectKey}" \\
+//             -Dsonar.host.url="${SonarQubeAPI}" \\
+//             -X
+//         """
+//     }
     
-    echo "SonarQube Analysis Completed!"
+//     echo "SonarQube Analysis Completed!"
+// }
+
+def call(String SonarQubeAPI, String Projectname, String ProjectKey){
+  withSonarQubeEnv("${SonarQubeAPI}"){
+      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
+  }
 }
