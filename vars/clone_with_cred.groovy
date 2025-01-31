45a1a651b529e9e85720266ddf0cb4e76f807a43
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-31 16:05:39 +0530

    Update clone_with_cred.groovy

--- vars/clone_with_cred.groovy
+++ vars/clone_with_cred.groovy
@@ -7,6 +7,8 @@ def call(String Projectname, String branch) {
 
             # Clone the repository
             git clone --branch '${branch}' https://\${GIT_USERNAME}:\${GIT_TOKEN}@github.com/Eyepatch5263/${Projectname}.git
+            cd ${Projectname}
+            npm run install
         """
     }
 }
