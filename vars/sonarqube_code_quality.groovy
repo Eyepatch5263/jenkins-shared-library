cf67f3bcb5eaa3070afeacfc09569cbe1d0761e8
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-01-31 22:50:13 +0530

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,4 +1,12 @@
-def call(){
-  timeout(time: 1, unit: "MINUTES")
-  waitForQualityGate abortPipeline: false
+def call() {
+    timeout(time: 1, unit: "MINUTES") {
+        script {
+            def qualityGate = waitForQualityGate(abortPipeline: false)
+            if (qualityGate.status != 'OK') {
+                error "❌ SonarQube Quality Gate failed: ${qualityGate.status}"
+            } else {
+                echo "✅ SonarQube Quality Gate Passed!"
+            }
+        }
+    }
 }
