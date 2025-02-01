77715414f694425355a81129de3ab1d98d310c57
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-02-01 10:42:30 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,9 @@
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -Dsonar.scm.provider=git -X"
+    
+      sh """
+      cd /var/lib/jenkins/workspace/Codilio-CI/${Projectname}
+      $SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -Dsonar.scm.provider=git -X
+    """
   }
 }
