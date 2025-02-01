243a2e20a16dae968c9a92b7e2a066fe7f7b5980
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-02-02 03:57:49 +0530

    Update git_push.groovy

--- vars/git_push.groovy
+++ vars/git_push.groovy
@@ -5,7 +5,6 @@ def call(String Project, String branch) {
         sh """
             echo "Checking repository status:"
             git status
-
             echo "Configuring Git user:"
             git config --global user.email "pratyushpragyey.com"
             git config --global user.name "Eyepatch5263"
@@ -17,7 +16,7 @@ def call(String Project, String branch) {
             git commit -m "Updated environment variables" || echo "No changes to commit"
 
             echo "Pushing changes to GitHub:"
-            git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${Project}.git ${branch}
+            git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${GIT_USERNAME}/${Project}.git ${branch}
         """
     }
 }
