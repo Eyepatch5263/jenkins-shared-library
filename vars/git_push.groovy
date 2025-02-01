bb9fa003ea0f670f973b653bf706529e7d814b8f
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-02-02 03:43:15 +0530

    Update git_push.groovy

--- vars/git_push.groovy
+++ vars/git_push.groovy
@@ -1,17 +1,22 @@
-def call(String Project, String branch){
-  withCredentials([gitUsernamePassword(credentialsId: 'Github-Credentials', gitToolName: 'Default')]) {
-                        sh '''
-                        echo "Checking repository status: "
-                        git status
-                    
-                        echo "Adding changes to git: "
-                        git add .
-                        
-                        echo "Commiting changes: "
-                        git commit -m "Updated environment variables"
-                        
-                        echo "Pushing changes to github: "
-                        git push https://github.com/eyepatch5263/${Project}.git ${branch}
-                    '''
-  }
+def call(String Project, String branch) {
+    withCredentials([gitUsernamePassword(credentialsId: 'Github-Credentials', gitToolName: 'Default', 
+                                         usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
+        sh '''
+            echo "Checking repository status:"
+            git status
+
+            echo "Configuring Git user:"
+            git config --global user.email "pratyushpragyey.com"
+            git config --global user.name "Eyepatch5263"
+
+            echo "Adding changes to Git:"
+            git add .
+
+            echo "Committing changes:"
+            git commit -m "Updated environment variables" || echo "No changes to commit"
+
+            echo "Pushing changes to GitHub:"
+            git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${Project}.git ${branch}
+        '''
+    }
 }
