381caceef861a8a1e7239fec26db230d1470e2d2
pratyush <43774637+Eyepatch5263@users.noreply.github.com>
2025-02-02 03:52:59 +0530

    Update git_push.groovy

--- vars/git_push.groovy
+++ vars/git_push.groovy
@@ -1,7 +1,8 @@
 def call(String Project, String branch) {
-    withCredentials([gitUsernamePassword(credentialsId: 'Github-Credentials', gitToolName: 'Default', 
-                                         usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
-        sh """
+    withCredentials([usernamePassword(credentialsId: 'Github-Credentials', 
+                                      usernameVariable: 'GIT_USERNAME', 
+                                      passwordVariable: 'GIT_TOKEN')]) {
+        sh '''
             echo "Checking repository status:"
             git status
 
@@ -17,6 +18,6 @@ def call(String Project, String branch) {
 
             echo "Pushing changes to GitHub:"
             git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${Project}.git ${branch}
-        """
+        '''
     }
 }
